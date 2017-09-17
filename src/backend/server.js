f1078548c322a60849b420a14b71e34a7e6e02ab
Allen <allen@allen.com>
2017-09-17 13:29:08 +0000

    make commits link to show their files

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,20 +2,28 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const dots = require('dot').process({path: './src/backend/templates'});
+const _ = require('lodash');
 const git = require('./git');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const projectPath = path.resolve(__dirname, '../../repos');
   const projects = fs.readdirSync(projectPath);
   res.send(dots.projects({projects}));
 });
 
-app.get('/:project', async (req, res) => {
+app.get('/:project/:commit?', async (req, res) => {
   const commits = await git.getCommits(req.params.project);
-  const files = await git.getFiles(commits[0]);
-  res.send(dots.project({commits, files}));
+
+  const commit = req.params.commit ?
+    _.find(commits, c => c.sha() == req.params.commit) :
+    _.last(commits);
+
+  const files = await git.getFiles(commit);
+  const templateVars = {commits, commit, files, project: req.params.project};
+
+  res.send(dots.project(templateVars));
 });
 
 module.exports = app;
