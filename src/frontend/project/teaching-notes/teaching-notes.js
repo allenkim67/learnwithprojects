55db07a3271c11648cf382c061b70067c4643584
Allen Kim <allenkim67@gmail.com>
2017-10-03 08:50:38 +0700

    style teaching notes

--- src/frontend/project/teaching-notes/teaching-notes.js
+++ src/frontend/project/teaching-notes/teaching-notes.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Markdown from 'react-markdown'
+import styles from './teaching-notes.css'
 
 export default class TeachingNotes extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Notes</h2>
+      <div className={styles.container}>
         <Markdown source={this.props.teachingNotes}/>
       </div>
     );
