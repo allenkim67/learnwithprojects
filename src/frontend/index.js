077437993ac215396a06d36154ef1432362fbfd4
Allen Kim <allenkim67@gmail.com>
2017-10-27 10:54:42 +0700

    add analytics for react router and only in prod

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,9 +5,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Project from './project/project'
+import Analytics from './analytics'
+
+const dev = location.hostname === '127.0.0.1' || location.hostname === 'localhost';
 
 const app = <BrowserRouter>
-  <Route path="/:project/:lang/:commit?" component={Project}/>
+  <div>
+    {dev ? null : <Route path="/" component={Analytics}/>}
+    <Route path="/:project/:lang/:commit?" component={Project}/>
+  </div>
 </BrowserRouter>;
 
 ReactDOM.render(app, document.getElementById('root'));
