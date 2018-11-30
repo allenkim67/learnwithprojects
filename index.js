959c4da43c420297d9f48a6e492b2afdbe2588c6
Allen Kim <allenkim67@gmail.com>
2018-11-30 19:59:07 +0700

    use pm2 for deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./src/backend/server');
 
-const PORT = process.env.NODE_ENV === 'production' ? 80: 3000;
+const PORT = process.env.NODE_ENV === 'production' ? 8082: 3000;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`)
